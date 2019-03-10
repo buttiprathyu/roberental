b31488363de09893a652273f60998b6314d5b739
royan.tuscano@gmail.com <royan.tuscano@gmail.com>
2019-03-10 12:10:40 -0400

    added app.js with encryption and added bcrypt module for encryption

--- server/app.js
+++ server/app.js
@@ -1,14 +1,98 @@
-const express= require('express');
-const bodyParser= require('body-parser');
-const app=express();
-const mongoose=require('mongoose');
-const url = 'mongodb://localhost:27017/robe';
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/robe'; //without authentication
+//const url ='mongodb://localhost:27017/robedata';
+var bcrypt = require('bcrypt');
 const User = require('./model/user');
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended : false}))
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
+const saltrounds = 3;
+//app.listen(3000, () => console.log('Blog server running on port 3000!'))
+
+app.post('/api/signup', (req, res) => {
+	console.log("Hekki");
+	mongoose.connect(url, function (err) {
+		if (err) throw err;
+
+		var password = req.body.password;
+
+		bcrypt.hash(password, saltrounds)
+			.then(function (hashedPassword) {
+				console.log("saving hashed password");
+				const user = new User({
+					email: req.body.email,
+					password: hashedPassword
+				})
+				user.save((err, res) => {
+					console.log(err);
+					if (err) throw err;
+					console.log("saving");
+				})
+			})
+			.then(function () {
+				res.send();
+			})
+			.catch(function (error) {
+				console.log("Error saving user: ");
+				console.log(error);
+				next();
+			});
+
+
+		console.log("Update");
+		return res.status(200).json({
+			successMsg: 'Successfully Created',
+			token: 'OK'
+		})
+	});
+})
+
+
+
+app.post('/api/login', (req, res) => {
+	mongoose.connect(url, {
+		useNewUrlParser: true
+	}, function (err) {
+		if (err) throw err;
+		var myemail = req.body.email;
+		var mypassword = req.body.password;
+		User.findOne({
+			email: myemail
+		}, function (err, user) {
+			if (err) throw err;
+			else if (!user) {
+				var err = new Error('User not found.');
+				err.status = 401;
+				return callback(err);
+			}
+			bcrypt.compare(mypassword, user.password, function (err, result) {
+				if (result == true) {
+					console.log("Success\n");
+					return res.status(200).json({
+						successMsg: 'Successfully login',
+						token: 'OK'
+					})
+				} else {
+					console.log("Incorrect Password");
+					return res.status(400).json({
+						errorMsg: 'Incorrect Password'
+					})
+				}
+
+			})
+		})
+	});
+
+})
+
+app.listen(3000, () => console.log('Blog server running on port 3000!'))
+/*
 app.post('/api/login', (req, res) => {
 
 	mongoose.connect(url,{ useNewUrlParser: true } , function(err){
@@ -36,6 +120,7 @@ app.post('/api/login', (req, res) => {
 	
 })
 
+/*
 app.post('/api/signup', (req, res) => {
 	mongoose.connect(url, function(err){
 		if(err) throw err;
@@ -55,8 +140,10 @@ app.post('/api/signup', (req, res) => {
 		})
 	});
 })
+*/
 
-app.listen(3000, () => console.log('Blog server running on port 3000!'))
+//app.listen(3000, () => console.log('Blog server running on port 3000!'))
+
 /*
 User.findOne({ username: 'jmar777' }, function(err, user) {
     if (err) throw err;
