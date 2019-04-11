5c3fa3bafb8e8f490bede9e7ea1eb39316316894
royan.tuscano@gmail.com <royan.tuscano@gmail.com>
2019-04-10 20:45:01 -0700

    cart functionality added

--- server/app.js
+++ server/app.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/robe'; //without authentication
+const fs = require('fs')
 //const url ='mongodb://localhost:27017/robedata';
 var bcrypt = require('bcrypt');
 var multer = require('multer');
-var img_dir = "/WorkSpace/MS CS/CSC 536 - SE/RobeRentalProject/robe-rental-app/src/assets"
+var img_dir = "../robe-rental-app/src/assets"
 //var img_dir = '/home/roy/Desktop/software_eng_proj/roberental/robe-rental-app/src/assets';
+
 var upload = multer({
 	dest: img_dir
 });
@@ -21,15 +23,12 @@ app.use(bodyParser.urlencoded({
 }))
 
 const saltrounds = 3;
-//app.listen(3000, () => console.log('Blog server running on port 3000!'))
 
 app.post('/api/signup', (req, res) => {
 	console.log("Hekki");
 	mongoose.connect(url, function (err) {
 		if (err) throw err;
-
 		var password = req.body.password;
-
 		bcrypt.hash(password, saltrounds)
 			.then(function (hashedPassword) {
 				console.log("saving hashed password");
@@ -105,17 +104,82 @@ app.get('/api/robeList', (req, res) => {
 	mongoose.connect(url, {
 		useNewUrlParser: true
 	}, function (err) {
-		Robe_image.find({}, function (err, all_images) {
-			if (err) throw err;
-			else if (!all_images) {
-				var err = new Error('No images');
-				err.status = 401;
-				return callback(err);
-			}
-			console.log("show all images");
-			console.log(all_images);
-			res.send(all_images);
-		})
+		//	console.log(req);
+		if (req.query.price == "low") {
+			console.log("Low call");
+			Robe_image.find().sort({
+				"rentPrice": -1,
+				"buyPrice": -1
+			}).exec(function (err, all_images) {
+				if (err) throw err;
+				else if (!all_images) {
+					var err = new Error('No images');
+					err.status = 401;
+					return callback(err);
+				}
+				console.log("show ascending images");
+				//	console.log(all_images);
+				res.send(all_images);
+			})
+		} else if (req.query.price == "high") {
+			console.log("High call");
+			Robe_image.find().sort({
+				"rentPrice": 1,
+				"buyPrice": 1
+			}).exec(function (err, all_images) {
+				if (err) throw err;
+				else if (!all_images) {
+					var err = new Error('No images');
+					err.status = 401;
+					return callback(err);
+				}
+				console.log("show descending images");
+				//		console.log(all_images);
+				res.send(all_images);
+			})
+		} else if (req.query.size == "S" || req.query.size == "M" || req.query.size == "L") {
+			console.log("Small call");
+			Robe_image.find({
+				"robeSize": req.query.size
+			}).exec(function (err, all_images) {
+				if (err) throw err;
+				else if (!all_images) {
+					var err = new Error('No images');
+					err.status = 401;
+					return callback(err);
+				}
+				console.log("show descending images");
+				//		console.log(all_images);
+				res.send(all_images);
+			})
+		} else if (req.query.size == "S,M,L") {
+			console.log("S/M/L call");
+			Robe_image.find({
+				"robeSize": ['S', 'M', 'L']
+			}).exec(function (err, all_images) {
+				if (err) throw err;
+				else if (!all_images) {
+					var err = new Error('No images');
+					err.status = 401;
+					return callback(err);
+				}
+				console.log("show descending images");
+				//		console.log(all_images);
+				res.send(all_images);
+			})
+		} else {
+			Robe_image.find({}, function (err, all_images) {
+				if (err) throw err;
+				else if (!all_images) {
+					var err = new Error('No images');
+					err.status = 401;
+					return callback(err);
+				}
+				console.log("show all images");
+				//		console.log(all_images);
+				res.send(all_images);
+			})
+		}
 	});
 })
 
@@ -123,10 +187,10 @@ app.post('/api/imgupload', upload.single('robeImg'), (req, res) => {
 	mongoose.connect(url, {
 		useNewUrlParser: true
 	}, function (err) {
-		console.log("The post request");
+		//	console.log("The post request");
 		var file_path = 'assets/' + req.file.filename;
-		console.log(file_path);
-		console.log(req.file.path);
+		//	console.log(file_path);
+		//	console.log(req.file.path);
 		const robe_image = new Robe_image({
 			email: req.body.email,
 			robeTitle: req.body.robeTitle,
@@ -156,18 +220,29 @@ app.post('/api/myaccount', (req, res) => {
 	}, function (err) {
 		if (err) throw err;
 		var myemail = req.body.email;
-		var myquery = { email: req.body.email };
-		var newvalues = { $set: {firstname: req.body.firstname, lastname: req.body.lastname, address1:req.body.address1,
-			address2:req.body.address2, city:req.body.city, state:req.body.state, zipcode: req.body.zipcode } };
-			User.updateOne(myquery, newvalues, function(err, res) {
-				if (err) throw err;
-				console.log("Account updated");
-			  });
-			  
-			  return res.status(200).json({
-				successMsg: 'Successfully Updated',
-				token: 'OK'
-			})
+		var myquery = {
+			email: req.body.email
+		};
+		var newvalues = {
+			$set: {
+				firstname: req.body.firstname,
+				lastname: req.body.lastname,
+				address1: req.body.address1,
+				address2: req.body.address2,
+				city: req.body.city,
+				state: req.body.state,
+				zipcode: req.body.zipcode
+			}
+		};
+		User.updateOne(myquery, newvalues, function (err, res) {
+			if (err) throw err;
+			console.log("Account updated");
+		});
+
+		return res.status(200).json({
+			successMsg: 'Successfully Updated',
+			token: 'OK'
+		})
 	});
 })
 
@@ -178,96 +253,80 @@ app.get('/api/myaccount', (req, res) => {
 		if (err) throw err;
 		var myemail = req.query.email;
 		console.log("In get call");
-		console.log(req);
-		console.log(myemail);
-		var myquery = { email: myemail };
-		User.findOne(myquery, function(err, result) {
+		//		console.log(req);
+		//		console.log(myemail);
+		var myquery = {
+			email: myemail
+		};
+		User.findOne(myquery, function (err, result) {
 			if (err) throw err;
 			console.log(result);
 			res.send(result);
-		  });
-		 
+		});
+
 	});
 })
 
-/*
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  firstname:{ type: String, required: true },
-  lastname:{ type: String, required: true },
-  address1:{ type: String},
-  address2:{ type: String},
-  city:{ type: String},
-  state:{ type: String},
-  zipcode:{ type: Number}
-*/
-app.listen(3000, () => console.log('Blog server running on port 3000!'))
-/*
-app.post('/api/login', (req, res) => {
-
-	mongoose.connect(url,{ useNewUrlParser: true } , function(err){
-		if(err) throw err;
-		User.find({
-			email : req.body.email, password : req.body.password
-		}, function(err, user){
-			if(err) throw err;
-			if(user.length === 1){	
-				console.log("Success\n");
-				return res.status(200).json({
-					successMsg:'Successfully login',
-					token:'OK'
-				})
-			} else {
-				console.log(req.body.email);
-				console.log("fail\n");
-				console.log(req.body.password);
-				return res.status(400).json({
-					errorMsg: 'Something went wrong. Please Try Again'
-				})
-			}
-		})
-	});
-	
-})
-
-/*
-app.post('/api/signup', (req, res) => {
-	mongoose.connect(url, function(err){
-		if(err) throw err;
-		const user = new User({
+app.post('/api/imgupload', upload.single('robeImg'), (req, res) => {
+	mongoose.connect(url, {
+		useNewUrlParser: true
+	}, function (err) {
+		//	console.log("The post request");
+		var file_path = 'assets/' + req.file.filename;
+		//	console.log(file_path);
+		//	console.log(req.file.path);
+		const robe_image = new Robe_image({
 			email: req.body.email,
-			password: req.body.password
-		})
-		console.log("Update");
-		user.save((err, res) => {
+			robeTitle: req.body.robeTitle,
+			robeImg: file_path,
+			rentPrice: req.body.rentPrice,
+			buyPrice: req.body.buyPrice,
+			robeSize: req.body.robeSize,
+			robeMaterialDesc: req.body.robeMaterialDesc,
+			robeMaterial: req.body.robeMaterial,
+			robeCare: req.body.robeCare
+		});
+		robe_image.save((err, res) => {
 			console.log(err);
-			if(err) throw err;
-			console.log("saving");
+			if (err) throw err;
+			console.log("Image saving");
 		})
 		return res.status(200).json({
-			successMsg: 'Successfully Created',
-			token:'OK'
+			successMsg: 'Successfully Uploaded',
+			token: 'OK'
 		})
 	});
 })
-*/
 
-//app.listen(3000, () => console.log('Blog server running on port 3000!'))
+app.post('/api/cart', (req, res) => {
+	mongoose.connect(url, {
+		useNewUrlParser: true
+	}, function (err) {
+		if (err) throw err;
+		console.log(req.body.robeList[0].robeImg);
+		console.log(req.body.robeList[0]._id);
+		//console.log(req.body.robeList[0].robeImg);
+		var mypath = '../robe-rental-app/src/' + req.body.robeList[0].robeImg;
+		//console.log(mypath);
+		Robe_image.findOneAndDelete({
+			"robeImg": req.body.robeList[0].robeImg
+		}, function (err, result) {
+			if (err) throw err;
+			console.log("Deleted bu query");
+		});
+		fs.unlink(mypath, (err) => {
+			if (err) {
+				console.error(err)
+				return
+			}
+			console.log("Successfully deleted");
+		});
 
-/*
-User.findOne({ username: 'jmar777' }, function(err, user) {
-    if (err) throw err;
+		return res.status(200).json({
+			successMsg: 'Successfully deleted',
+			token: 'OK'
+		})
 
-    // test a matching password
-    user.comparePassword('Password123', function(err, isMatch) {
-        if (err) throw err;
-        console.log('Password123:', isMatch); // -&gt; Password123: true
-    });
-
-    // test a failing password
-    user.comparePassword('123Password', function(err, isMatch) {
-        if (err) throw err;
-        console.log('123Password:', isMatch); // -&gt; 123Password: false
-    });
-});
-*/
+	});
+})
+app.listen(3000, () => console.log('Blog server running on port 3000!'))
